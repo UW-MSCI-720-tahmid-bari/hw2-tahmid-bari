6ee318ccbc4e1aded2c7c7576767cdf019a7b633
Tahmid Bari <tahmid_bari@hotmail.com>
2021-06-11 07:17:48 +0800

    Initial Commit

---
+++ getTestQueries.py
@@ -0,0 +1,5 @@
+import pickle
+
+topics = {1: 'electric car', 2: 'touchscreen display', 3: 'car accident', 4: 'coffee', 5: 'car'}
+
+pickle.dump(topics, open('C:/Users/Tahmid Bari/Desktop/UWaterloo/Study/MSCI_720/720_Assignments/720_HW2/~/MSCI_720/Documents/test_topics.p', 'wb'))
